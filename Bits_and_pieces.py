668c96b749560ef31d4c10a33da5ea9493162d5a
Arkleseisure <86984152+Arkleseisure@users.noreply.github.com>
2021-08-11 21:45:20 +0100

    File to hold random functions useful everywhere

---
+++ Bits_and_pieces.py
@@ -0,0 +1,16 @@
+﻿import math
+
+# function to convert an input bitboard to a readable square in text such as 'a1' or 'f3'
+def convert_to_text(pos):
+	# finds the square number of the position (see explanation in play_game)
+	try:
+		square_number = int(math.log2(pos))
+	except ValueError:
+		print(pos)
+	
+	# finds the row and column of the position
+	col = square_number % 8
+	row = square_number // 8
+
+	# returns the letter for the column and the number of the row as a string
+	return chr(col + 97) + str(row + 1)
