29a810fde74455fe00f39a0a121e29d5eb4f2e38
Arkleseisure <86984152+Arkleseisure@users.noreply.github.com>
2021-09-17 16:45:30 +0200

    Added get_engine move
    Added get_engine_move, as well as edited it so that it worked with the new Game struct way of generating moves

--- c_interface.py
+++ c_interface.py
@@ -1,15 +1,18 @@
-from ctypes import CDLL, c_ulonglong, c_int, c_char, Structure, c_bool
+from ctypes import CDLL, c_ulonglong, c_int, c_char, Structure, c_bool, c_double
 import sys
 import random
 import time
-import Bits_and_pieces as BaP
+from Bits_and_pieces import get_bitboard_from_fen, convert_to_text
 from draw_board import print_board
 
 
 # imports the c libraries
 game_mechanics_lib_path = 'theories/game_mechanics_%s.so' % (sys.platform)
 game_mech = CDLL(game_mechanics_lib_path)
+engine_lib_path = 'theories/engine_%s.so' % (sys.platform)
+engine = CDLL(engine_lib_path)
 game_mech.confirm_it_works()
+engine.check_it_works()
 
 
 class Piece(Structure):
@@ -22,9 +25,16 @@ class Piece(Structure):
 		else:
 			colour = 'black '
 
-		location = BaP.convert_to_text(self.loc)
+		location = convert_to_text(self.loc)
 
 		return ('captured ' if self.captured else '') + colour + types[self.type % 6] + ' on ' + location
+
+
+class Game(Structure):
+	_fields_ = [('piece_list', (Piece * 32)), ('board', (c_ulonglong * 12)), ('hash', c_ulonglong), ('past_hash_list', (c_ulonglong * 100)), ('last_move', (c_ulonglong * 3)), 
+			 ('to_play', c_int), ('ply_counter', c_int), ('castling', c_int)]
+
+
 
 '''
 Interface for the apply function
@@ -41,28 +51,17 @@ but the move applied is already known to the program calling this function.
 Last Modified: 04/07/2021
 Last Modified by: Arkleseisure
 '''
-def apply(board, move, castling, to_play, piece_list, past_hash_list, current_hash, ply_counter, zobrist_numbers):
+def apply(game, move, zobrist_numbers):
 	# casts the python variables into the equivalent types in c
-	c_board = (c_ulonglong * 12)(*board)
-	c_move = (c_ulonglong * 3)(*move)
-	c_last_move = (c_ulonglong * 3)()
-	# extras is created so that the object passed is mutable, making it easier to
-	# return stuff
-	extras = (c_int * 3)(*[castling, to_play, ply_counter])
 	c_zobrist_numbers = (c_ulonglong * 793)(*zobrist_numbers)
-	c_hash = (c_ulonglong * 1)(*[current_hash])
 	c_removed_hash = (c_ulonglong * 1)()
+	c_move = (c_ulonglong * 3)(*move)
+	c_game = (Game * 1)(*[game])
 
-	piece_taken = game_mech.apply(c_board, c_move, extras, c_hash, c_last_move, piece_list, past_hash_list, c_zobrist_numbers, c_removed_hash)
-	extras = extras[:]
-	castling = extras[0]
-	to_play = extras[1]
-	ply_counter = extras[2]
-	hash = c_hash[0]
+	piece_taken = game_mech.apply(c_game, c_move, c_zobrist_numbers, c_removed_hash)
 
-	# returns the c arrays as python lists ([:] will automatically cast an
-	# iterable into a list)
-	return c_board[:], castling, to_play, c_last_move[:], hash, ply_counter
+	return c_game[0]
+	
 
 '''
 Interface for the legal_moves function
@@ -74,62 +73,37 @@ last_move = last move to be played as 2 bitboards and a flag
 piece_list = length 32 array containing structs with all the pieces and their locations for quicker move generation
 OUTPUTS:
 legal_moves list containing all the legal moves in the position as c arrays of 2 bitboards and a flag (documented in play_game)
-Last Modified: 2/9/2021
+Last Modified: 16/9/2021
 Last Modified by: Arkleseisure
 '''
-def legal_moves(board, castling, to_play, last_move, piece_list):
-	# casts the variables into equivalent types in c
-	c_board = (c_ulonglong * 12)(*board)
-	c_last_move = (c_ulonglong * 3)(*last_move)
-	c_to_play = c_int(to_play)
-	c_castling = c_int(castling)
+def legal_moves(game):
 	# I think the maximum possible number of legal moves from a single position is about this
 	# (https://lichess.org/analysis/fromPosition/R6R/3Q4/1Q4Q1/4Q3/2Q4Q/Q4Q2/pp1Q4/kBNN1KB1_w_-_-)
 	c_legal_moves = ((c_ulonglong * 3) * 220)()
+	c_game = (Game * 1)(*[game])
 
-	num_moves = game_mech.legal_moves(c_board, c_legal_moves, c_castling, c_to_play, c_last_move, piece_list)
+	num_moves = game_mech.legal_moves(c_game, c_legal_moves)
 	return c_legal_moves[:num_moves]
 
-# initiates the piece list, an array containing a struct for each piece
-def initiate_piece_list(board):
-	piece_types = {0: 'white pawn', 1: 'white knight', 2: 'white bishop', 3: 'white rook', 4: 'white queen', 5: 'white king', 
-			   6: 'black pawn', 7: 'black knight', 8: 'black bishop', 9: 'black rook', 10: 'black queen', 11: 'black king'}
-	piece_list = (Piece * 32)()
-	piece_num = 0
-	try:
-		for i in range(12):
-			for j in range(64):
-				if ((2 ** j) & board[i]) != 0:
-					new_piece = Piece()
-					new_piece.loc = 2 ** j
-					new_piece.type = i
-					new_piece.captured = False
-					piece_list[piece_num] = new_piece
-					piece_num += 1
-		
-			# ensures that the kings are always the 16th and 32nd in the piece_list as this fact is used in game_mechanics
-			if i % 6 == 4 and piece_num % 16 != 15:
-				for j in range(15 - (piece_num % 16)):
-					new_piece = Piece()
-					new_piece.loc = 0
-					new_piece.type = 0
-					new_piece.captured = True
-					piece_list[piece_num] = new_piece
-					piece_num += 1
-			# does the same for the rooks, as this is used for quick location of the rooks when castling
-			elif i % 6 == 2 and piece_num % 16 != 12:
-				for j in range(12 - (piece_num % 16)):
-					new_piece = Piece()
-					new_piece.loc = 0
-					new_piece.type = 0
-					new_piece.captured = True
-					piece_list[piece_num] = new_piece
-					piece_num += 1
-	except IndexError:
-		print(piece_num)
-		print_board(board)
 
-	return piece_list
+def get_engine_move(game, zobrist_numbers, time_allowed):
+	c_game = (Game * 1)(*[game])
+	c_zobrist_numbers = (c_ulonglong * 793)(*zobrist_numbers)
+	depth = 1
+	move_number = (c_int * 1)(*[0])
+	c_time_allowed = c_double(time_allowed)
+	current_value = 0
+	move = [0, 0, 0]
+	moves = legal_moves(game)
+
+	start_time = time.time()
+	value = engine.get_engine_move(c_game, c_zobrist_numbers, move_number, c_time_allowed)
+	if time.time() - start_time < time_allowed:
+		print('Not enough time was spent, there is a glitch.')
+		quit()
+
+	move = moves[move_number[0]]
+	return move, current_value
 
 '''
 Generates pseudorandom numbers used for zobrist hashes (an efficient way to hash a board position), 
@@ -139,7 +113,7 @@ More info on Zobrist Hashing here: https://www.chessprogramming.org/Zobrist_Hash
 Last Modified: 19/8/2021
 Last Modified by: Arkleseisure
 '''
-def generate_zobrist_stuff(board, castling, to_play, last_move):
+def generate_zobrist_stuff(game):
 	# ensures that the numbers are the same each time the program is run
 	random.seed(1)
 
@@ -152,51 +126,123 @@ def generate_zobrist_stuff(board, castling, to_play, last_move):
 	for i in range(793):
 		zobrist_numbers.append(random.getrandbits(64))
 
-	initial_hash = 0
+	game.hash = 0
 	# the zobrist hash is made by xoring the zobrist numbers corresponding to each
 	# item of the game
 	# adds the pieces from the board
-	for i in range(len(board)):
-		binary_representation = bin(board[i])
+	for i in range(len(game.board)):
+		binary_representation = bin(game.board[i])
 		index = 0
 		while (index != -1):
 			index = binary_representation.find('1', index + 1)
 			if index != -1:
-				initial_hash ^= zobrist_numbers[64 * i + len(binary_representation) - index - 1]
+				game.hash ^= zobrist_numbers[64 * i + len(binary_representation) - index - 1]
 
 	# adds the castling legality
-	initial_hash ^= zobrist_numbers[64 * 12 + castling]
+	game.hash ^= zobrist_numbers[64 * 12 + game.castling]
 
 	# adds en-passant
 	# checks if the last move was a double pawn move
-	if last_move[-1] ^ 15 == 1:
-		last_move_file = game_mech.file(last_move[1])
+	if game.last_move[-1] ^ 15 == 1:
+		last_move_file = game_mech.file(game.last_move[1])
 		
 		# checks if there is a pawn on the squares next to the last move played
-		if ((last_move[1] >> 1) & board[6 * to_play] != 0 and last_move_file > 0) or \
-			((last_move[1] << 1) & board[6 * to_play] != 0 and last_move_file < 7):
-			initial_hash ^= zobrist_numbers[64 * 12 + 16 + last_move_file]
+		if ((game.last_move[1] >> 1) & game.board[6 * game.to_play] != 0 and last_move_file > 0) or \
+			((game.last_move[1] << 1) & game.board[6 * game.to_play] != 0 and last_move_file < 7):
+			game.hash ^= zobrist_numbers[64 * 12 + 16 + last_move_file]
 
 	# adds person to move
-	if to_play == 1:
-		initial_hash ^= zobrist_numbers[64 * 12 + 16 + 8]
+	if game.to_play == 1:
+		game.hash ^= zobrist_numbers[64 * 12 + 16 + 8]
 
-	past_hash_list = (c_ulonglong * 100)()
-	past_hash_list[0] = c_ulonglong(initial_hash)
+	game.past_hash_list[0] = c_ulonglong(game.hash)
 
-	return initial_hash, zobrist_numbers, past_hash_list
+	return zobrist_numbers
+
+def get_board(game, board_fen):
+	white_pawns = get_bitboard_from_fen(board_fen, 'P')
+	white_knights = get_bitboard_from_fen(board_fen, 'N')
+	white_bishops = get_bitboard_from_fen(board_fen, 'B')
+	white_rooks = get_bitboard_from_fen(board_fen, 'R')
+	white_queens = get_bitboard_from_fen(board_fen, 'Q')
+	white_king = get_bitboard_from_fen(board_fen, 'K')
+
+	black_pawns = get_bitboard_from_fen(board_fen, 'p')
+	black_knights = get_bitboard_from_fen(board_fen, 'n')
+	black_bishops = get_bitboard_from_fen(board_fen, 'b')
+	black_rooks = get_bitboard_from_fen(board_fen, 'r')
+	black_queens = get_bitboard_from_fen(board_fen, 'q')
+	black_king = get_bitboard_from_fen(board_fen, 'k')
+
+	game.board = (c_ulonglong * 12)(*[white_pawns, white_knights, white_bishops, white_rooks, white_queens, white_king, 
+                black_pawns, black_knights, black_bishops, black_rooks, black_queens, black_king])
 
 
-def perft(board, last_move, castling, piece_list, to_play, initial_hash, past_hash_list, ply_counter, zobrist_numbers, depth,  type='all'):
-	c_board = (c_ulonglong * 12)(*board)
-	c_last_move = (c_ulonglong * 3)(*last_move)
-	c_castling = c_int(castling)
-	c_to_play = c_int(to_play)
-	c_initial_hash = c_ulonglong(initial_hash)
-	c_ply_counter = c_int(ply_counter)
+# initiates the piece list, an array containing a struct for each piece
+def initiate_piece_list(game):
+	piece_types = {0: 'white pawn', 1: 'white knight', 2: 'white bishop', 3: 'white rook', 4: 'white queen', 5: 'white king', 
+				6: 'black pawn', 7: 'black knight', 8: 'black bishop', 9: 'black rook', 10: 'black queen', 11: 'black king'}
+
+	piece_num = 0
+	try:
+		for i in range(12):
+			for j in range(64):
+				if ((2 ** j) & game.board[i]) != 0:
+					new_piece = Piece()
+					new_piece.loc = 2 ** j
+					new_piece.type = i
+					new_piece.captured = False
+					game.piece_list[piece_num] = new_piece
+					piece_num += 1
+		
+			# ensures that the kings are always the 16th and 32nd in the piece_list as this fact is used in game_mechanics
+			if i % 6 == 4 and piece_num % 16 != 15:
+				for j in range(15 - (piece_num % 16)):
+					new_piece = Piece()
+					new_piece.loc = 0
+					new_piece.type = 0
+					new_piece.captured = True
+					game.piece_list[piece_num] = new_piece
+					piece_num += 1
+			# does the same for the rooks, as this is used for quick location of the rooks when castling
+			elif i % 6 == 2 and piece_num % 16 != 12:
+				for j in range(12 - (piece_num % 16)):
+					new_piece = Piece()
+					new_piece.loc = 0
+					new_piece.type = 0
+					new_piece.captured = True
+					game.piece_list[piece_num] = new_piece
+					piece_num += 1
+	except IndexError:
+		print(piece_num)
+		print_board(game.board)
+
+def get_last_move(game, en_passant_target):
+    last_from = 0
+    last_to = 0
+    last_flag = 0
+    if en_passant_target != '-':
+        last_flag = 2
+        col = ord(en_passant_target[0]) - ord('a')
+        row = int(en_passant_target[1]) - 1
+
+        square_num = 8 * row + col
+        target_square_bitboard = 2 ** square_num
+
+        if to_play == 0:
+            last_from = target_square_bitboard << 8
+            last_to = target - square_bitboard >> 8
+        else:
+            last_from = target_square_bitboard >> 8
+            last_to = target_square_bitboard << 8
+
+    game.last_move = (c_ulonglong * 3)(*[last_from, last_to, last_flag])
+
+
+def perft(game, zobrist_numbers, depth, type='all'):
 	c_depth = c_int(depth)
 	c_zobrist_numbers = (c_ulonglong * 793)(*zobrist_numbers)
-	c_past_hash_list = (c_ulonglong * 100)(*past_hash_list)
+	c_game = (Game * 1)(*[game])
 	answer_type = ['Nodes', 'Captures', 'En passant', 'Castling', 'Promotion', 'Checks', 'Checkmates']
 	answer_dict = {}
 
@@ -205,12 +251,13 @@ def perft(board, last_move, castling, piece_list, to_play, initial_hash, past_ha
 	if type == 'all':
 		answers = (c_ulonglong * 7)()
 		start_time = time.time()
-		game_mech.perft_all(c_board, c_last_move, c_past_hash_list, c_castling, piece_list, c_to_play, c_initial_hash, c_ply_counter, c_depth, answers, c_zobrist_numbers)
+		game_mech.perft_all(c_game, answers, c_zobrist_numbers, c_depth)
 		time_taken = time.time() - start_time
+		game = c_game[0]
 	else:
 		answers = (c_ulonglong * 1)()
 		start_time = time.time()
-		game_mech.perft_nodes(c_board, c_last_move, c_castling, piece_list, c_to_play, c_initial_hash, c_past_hash_list, c_ply_counter, c_depth, answers, c_zobrist_numbers)
+		game_mech.perft_nodes(c_game, c_depth, answers, c_zobrist_numbers)
 		time_taken = time.time() - start_time
 
 	for i in range(len(answers)):
@@ -219,11 +266,6 @@ def perft(board, last_move, castling, piece_list, to_play, initial_hash, past_ha
 	return answer_dict, time_taken
 
 
-def terminal(board, to_play, piece_list, ply_counter, past_hash_list, current_hash, last_move):
-	c_board = (c_ulonglong * 12)(*board)
-	c_to_play = c_int(to_play)
-	c_ply_counter = c_int(ply_counter)
-	c_hash = c_ulonglong(current_hash)
-	c_last_move = (c_ulonglong * 3)(*last_move)
-
-	return int(game_mech.terminal(c_board, c_to_play, piece_list, c_ply_counter, past_hash_list, c_hash, c_last_move))
+def terminal(game):
+	c_game = (Game * 1)(*[game])
+	return int(game_mech.terminal(c_game))
